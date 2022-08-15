f76eff61e8d7f6345403564bf1b7df1a08f24ae4
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-14 20:25:02 -0700

    commit tree d938ff3d4ce933882a86d73d69ad0cf2dcb92ca5

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+    if (someValue <= 400) {
+      return "This one is on me!";
+    }
+    else if (someValue <=2000) {
+      return "That will be twenty bucks."
+    }
+    else if (someValue <=2500) {
+      return "I will gladly take your thirty bucks."
+    }
+    else {
+      return "No can do."
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return (someCity === 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  switch (someTip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;  
+  }
 }
